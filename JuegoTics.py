74a1eefc930ffa194c5b29224edc060faee8b1d2
IgnacioCourt <icourt@alumnos.uai.cl>
2022-03-15 00:30:56 -0300

    Commit 5

--- JuegoTics.py
+++ JuegoTics.py
@@ -13,3 +13,19 @@ while a == c and b==d:
 terreno[a][b]=1
 terreno[c][d]=1
 print(terreno)
+jugar=str(input("¿Quieres jugar BattleShip? (responde s si quieres y n si no)"))
+if jugar == "s":
+    print("Exíste un Area maritima en forma de matris de 3x3, en donde se ocultan 2 naves enemigas, intenta destruirlas lanzandoles bombas.\n")
+    while np.sum(terreno)>0:
+        bomba_fila=int(input("Si las filas van de arriba a abajo y comienzan desde la fila 0 y terminan en la 2. \n ¿En que fila crees que se encuentra alguna de las naves enemigas?"))
+        bomba_col=int(input("Si las columnas van de izquierda a derecha y comienzan desde la columna 0 y terminan en la 2. \n ¿En que columna crees que se encuentra alguna de las naves enemigas?"))
+        if terreno[a][b]==terreno[bomba_fila][bomba_col] or terreno[c][d]==terreno[bomba_fila][bomba_col]:
+            print("Felicitaciones, has destruido una nave enemiga")
+            terreno[bomba_fila][bomba_col]=0
+        else:
+            print("Lo siento, has errado, intentalo denuevo")
+    print("Felicitaciones, has destruido todas las naves enemigas")
+elif jugar=="n":
+    print("Será para la proxima")
+else:
+    print("Respuesta no válida")
