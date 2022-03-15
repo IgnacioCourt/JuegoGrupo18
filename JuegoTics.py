541717cd36ab2b111340a3878fcd8e91be3db966
IgnacioCourt <icourt@alumnos.uai.cl>
2022-03-14 23:36:38 -0300

    Commit 2

--- JuegoTics.py
+++ JuegoTics.py
@@ -1,1 +1,15 @@
 print("BattleShip")
+import numpy as np
+from random import choice
+
+terreno=np.array(3,3,0)
+
+a=choice([1,2,3])
+b=choice([1,2,3])
+c=choice([1,2,3])
+d=choice([1,2,3])
+while a == c and b==d:
+    b=choice([1,2,3])
+terreno[a,b]=1
+terreno[c,d]=1
+print(terreno)
